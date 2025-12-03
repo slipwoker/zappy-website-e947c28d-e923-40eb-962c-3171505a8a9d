d217a9d1840ba4123d7a41ce4efa9d822e2b3ea8
Maor Sharon <maor@zappy5.com>
2025-12-03 17:26:40 +0200

    Switch to version 2: Initial website generation

--- script.js
+++ script.js
@@ -47,7 +47,7 @@ document.addEventListener('DOMContentLoaded', function() {
   const phoneHeaderBtn = document.querySelector('.phone-header-btn');
   if (phoneHeaderBtn) {
     phoneHeaderBtn.addEventListener('click', function() {
-      const phoneNumber = '+972543951940';
+      const phoneNumber = '[business_phone]';
       window.location.href = 'tel:' + phoneNumber;
     });
   }
